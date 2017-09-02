4bff67340bf0fe52a42ff0e106329470654112a3
Shady <kingshady@gmail.com>
2017-09-02 22:57:58 +0400

    StringType

---
+++ StringType/StringType/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // \' \" \\ \0 \a \b \n \r \t \v \f
+            //@
+
+            string myString ="Nazar said This is a good course, and i think";
+            string myString2 = @" Yes it is";
+            string MyString3 = myString + myString2;
+            Console.WriteLine("{0}", MyString3);
+
+            Console.WriteLine( "{0}", myString);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
