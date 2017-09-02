77b5c32aeabf8511614446183a63dfa86e640592
Shady <kingshady@gmail.com>
2017-09-02 22:36:57 +0400

    Literal_Charcters

---
+++ Literal_Charcters/Literal_Charcters/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Literal_Charcters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar = -100;
+            uint myUVar = 1000U;
+            ulong myLVar = 1000;
+
+            float myFLVar = 1.5F;
+            double myDVar = 100.5d;
+            decimal myDecVar = 100.5M;
+
+            Console.WriteLine("{0}\n {1}\n {2}\n", myVar, myUVar, myLVar);
+
+            Console.WriteLine($"{myDecVar}\n{myDVar}\n{myFLVar}\n");
+
+            Console.ReadKey();
+
+        }
+    }
+}
