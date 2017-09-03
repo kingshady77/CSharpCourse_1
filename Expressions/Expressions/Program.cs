c5ef66e81ec95cf2cab28a323973d43e91259c5c
Shady <kingshady@gmail.com>
2017-09-03 22:27:13 +0400

    Expressions

---
+++ Expressions/Expressions/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float myVar = 10.5f;
+            float myVar2 = 10.7f;
+
+            float temp = myVar + myVar2;
+
+            // basic operators + - / *  
+            // bitwise operations
+            // logical operators Boolean true, false
+
+            //1- Unary operand
+            //2- Binary operand
+            //3- operands
+
+
+        }
+    }
+}
