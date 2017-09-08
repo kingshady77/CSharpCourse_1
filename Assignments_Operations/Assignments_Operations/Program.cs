9b60e80094dc996d421dc7220f69309413dfab74
Shady <kingshady@gmail.com>
2017-09-08 18:37:47 +0400

    Assignments_Operations

---
+++ Assignments_Operations/Assignments_Operations/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // =, +=, -=, /=, %=,
+
+            int myVar1 = 7, myVar2 = 3;
+
+           // myVar1 = myVar2;
+
+         //   myVar1 += myVar2;   // myVar1 = myVar1 + myVar2
+         //   myVar2 += myVar1;
+
+        //    myVar1 -= myVar2;     // myVar1 = myVar1 - myVar2
+
+            myVar1 %= myVar2;     //myVar1 = myVar1 % myVar2
+
+            Console.WriteLine("The Result is = {0}, The value of Var2 = {1}", myVar1, myVar2);
+            Console.ReadKey();
+             
+
+        }
+    }
+}
