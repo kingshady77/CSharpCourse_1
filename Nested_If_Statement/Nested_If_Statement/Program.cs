e9cb2ed03ac830e61b829f2b38b47409c6185581
Shady <kingshady@gmail.com>
2017-09-09 19:09:08 +0400

    Nested_If_Statement

---
+++ Nested_If_Statement/Nested_If_Statement/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_If_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar1 = 9, myVar2 = 6;
+
+            if (myVar1 == 10)
+            {
+                Console.WriteLine("myVar1 equal 9");
+                if (myVar2 == 7)
+                {
+                    Console.WriteLine("myVar2 ==7");
+                }
+                else
+                {
+                    Console.WriteLine("myVar2 is not equal 7");
+                }
+            }
+            else
+            {
+                Console.WriteLine("myVar1 not equal 9");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
