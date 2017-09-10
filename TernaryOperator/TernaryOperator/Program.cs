884f872a7d6535bb7b91aa8d1b263f7003679f5b
Shady <kingshady@gmail.com>
2017-09-10 22:02:05 +0400

    TernaryOperator

---
+++ TernaryOperator/TernaryOperator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TernaryOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int myVar = 9;
+
+            ////string myResult = (myVar < 8) ? "Less than 8" : "Greater than 8";
+            ////Console.WriteLine("My Variable Result is {0}", myResult);
+            ////Console.ReadKey();   
+
+            string myName = "nazar", result;
+            result = (myName.Equals("Nazar")) ? "Yes" : "No";
+            Console.WriteLine("My variable Result is {0}", result);
+            Console.ReadKey();
+
+        }
+    }
+}
