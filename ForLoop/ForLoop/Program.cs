b9b3f914407340d32e7b6c7b41c829a1cedc37c1
Shady <kingshady@gmail.com>
2017-09-11 00:51:04 +0400

    ForLoop

---
+++ ForLoop/ForLoop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mySalary = 1000, total = 0;
+            for (int i = 1; i <= 12; i++)
+            {
+                total += mySalary;
+                Console.WriteLine("{0} Month Salary = {1}", i, total);
+            }
+            Console.ReadKey();
+        }
+    }
+}
