97d09a382be1a5fa4a4974c297c27f838ba564de
Shady <kingshady@gmail.com>
2017-09-29 22:18:32 +0400

    Array_Foreach

---
+++ Array_Foreach/Array_Foreach/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MyArraySize = 6;
+            int[] MyNumbers = new int[MyArraySize] { 1, 5, 8, 9, 7, 10 };
+
+            for (int i = 0; i < MyArraySize; i++)
+                Console.WriteLine("My Numbers" + (i + 1) + " = " + $"{MyNumbers[i]}");
+            Console.WriteLine("\n");
+
+            // Other Option with Foreach
+            foreach (int item in MyNumbers)
+                Console.WriteLine("My Numbers " + $"{item}"); 
+
+
+        }
+    }
+}
