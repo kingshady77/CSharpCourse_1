aa69ef122c6ff031aac7379a442a0d48447e8588
Shady <kingshady@gmail.com>
2017-09-29 15:19:23 +0400

    Array_ForLoop

---
+++ Array_ForLoop/Array_ForLoop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int myArraySize = 5;
+            int[] myNumbers = { 1, 5, 8, 9, 7 };
+            int[] myNumbers2 = new int[myArraySize];
+            int[] myNumbers3 = new int[myArraySize] {1,5,8,9,7};
+
+            for (int i = 0; i < 5; i++)
+                //   Console.WriteLine($"{myNumbers[i]}");
+                Console.WriteLine("My numbers" + (i+1) + " = " + $"{myNumbers[i]}" + " ");
+        }
+    }
+}
