fbc155bd6234f58b969430f447828f3378e7fb8d
Shady <kingshady@gmail.com>
2017-09-29 23:21:30 +0400

    ArrayOfString

---
+++ ArrayOfString/ArrayOfString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayOfString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] MyFullName = { "Shady ", "Mohammed ", "Al Qudmany " };
+            int MyArrayLength = MyFullName.Length;
+            
+            foreach(string item in MyFullName)
+            {     
+            Console.Write($"{item}");
+            }
+            Console.WriteLine("\nMy Array Length is " + $"{ MyArrayLength}");
+
+        }
+    }
+}
