7e7f0d84ff8652b479d8305d2cb7af47100dbe80
Shady <kingshady@gmail.com>
2017-10-24 23:05:41 +0400

    Defualt_ParametrizeConstructor

---
+++ Defualt_ParametrizeConstructor/Defualt_ParametrizeConstructor/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Defualt_ParametrizeConstructor
+{
+    class Person
+    {
+        string name;
+        int age;
+        public void PrintValues()
+        {
+            Console.WriteLine($"{name}, {age}");
+        }
+
+        // defualt constructor
+        public Person()
+        {
+            name = "Nizar Al Wattar";
+            age = 25;
+        }
+
+        // Parametrized constructor
+        public Person(string name)
+        {
+            this.name = name;
+            age = 30;
+           
+        }
+        
+        // Parametrized constructor
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+
+
+}
---
+++ Defualt_ParametrizeConstructor/Defualt_ParametrizeConstructor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Defualt_ParametrizeConstructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName2 = "Ahmed Ali", myName3 = "Shady";
+            int age2 = 30;
+            Person myObj = new Person();
+            {
+                myObj.PrintValues();
+            }
+
+            Console.WriteLine();
+            Person myObj2 = new Person(myName2, age2);
+            {
+                myObj2.PrintValues();
+            }
+
+
+            Console.WriteLine();
+            Person myObj3 = new Person(myName3);
+            {
+                myObj3.PrintValues();
+            }
+        }
+    }
+}
