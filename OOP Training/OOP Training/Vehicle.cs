ef9cf4dfc9cb9c6c47a9bb4f98c5ea00dc2e39e7
Shady <kingshady@gmail.com>
2017-10-24 17:02:16 +0400

    OOP_Training_1

---
+++ OOP Training/OOP Training/Vehicle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOP_Training
+{
+    /// <summary>
+    /// The is the parent class
+    /// </summary>
+    /// <remarks>This is a vehicle class</remarks>
+    public class Vehicle
+    {
+        /// <summary>
+        /// This value represent the car company
+        /// </summary>
+        /// <remarks>The company name</remarks>
+        /// <value>Cars Company Names</value>
+        public string manufacturer;
+        public string model;
+        public string color;
+        public int enginerSize;
+
+        /// <summary>
+        /// color fucntion
+        /// </summary>
+        /// <remarks>To change the car color</remarks>
+        public void painting()
+        {
+        }
+
+        /// <remarks>To specify the moving apporach</remarks>
+        public new void move()
+        {
+        }
+    }
+}
