15728cfd8bf52c5ab73c36422d8854c6ed15fedf
Shady <kingshady@gmail.com>
2017-10-25 22:51:11 +0400

    class26

---
+++ class26/class26/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             square mysquere = new square(17);
+
+            // mysquere. = 17;
+
+            Console.WriteLine("call: " + mysquere.call(4,3,4));
+      //      Console.Write("\nArea: " + mysquere.Area());
+        //    Console.WriteLine("\nLength: " + mysquere.Length());
+          //  Console.WriteLine("call: " + mysquere.call(15));
+        }
+    }
+}
---
+++ class26/class26/square.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace class26
+{
+    public class square
+    {
+        private const double pi = 3.4;
+        public int id;
+
+        public square(int linea)
+        {
+            this.Linea = linea;
+        }
+
+        public square()
+        {
+            this.Linea = 5;
+        }
+
+        ~square()
+        {
+            System.Diagnostics.Trace.WriteLine("decostructor");
+        }
+
+        /// <summary>
+        /// represent square length
+        /// </summary>
+        /// <remarks>square</remarks>
+        /// <value>0</value>
+        private int Linea
+        {
+            get;
+
+            set;
+        }
+
+        public int Area()
+        {
+            return (Linea * Linea);
+        }
+
+        public double Length()
+        {
+            return (Linea * 4*pi);
+        }
+
+        /// <summary>
+        /// represnet a square line
+        /// </summary>
+        //public int call2(int x)
+        //{
+        //    return (Linea * x);
+        //}
+
+        public int call(int x)
+        {
+            return (x * 5);
+        }
+
+        public int call(int x, int y)
+        {
+            return (x * y * 5);
+
+        }
+
+        public int call(int x, int y, int z)
+        {
+            return (x * y * z * 5);
+        }
+    }
+}
